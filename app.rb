504e286b1380bee49c4c13c761c9e9693acfb2a0
Steven Lotito <steven.lotito@alumni.pitt.edu>
2013-10-05 13:09:25 -0700

    Switched to rackup and added redis

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
-require 'redis'
 require 'json'
 
-set :port, 4000
+configure do
+  require 'redis'
+  redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+  uri = URI.parse(redisUri) 
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
 
 get '/getData' do
   { "http://fashionhack.com:4567/tumblr_mu5nuwmOMB1sk4zhuo1_500.jpg_707_500" => "http://www.betabrand.com/mens/pants/mens-japants-cargo-pants.html"}.to_json
