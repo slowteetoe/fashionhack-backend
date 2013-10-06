063a4da28e334f5b2f94b5a5d7fd8270eca28382
Steven Lotito <steven.lotito@alumni.pitt.edu>
2013-10-06 13:33:47 -0700

    Switching to redis

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 
 configure do
+  set :protection, :except => [:http_origin]
   require 'redis'
   redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
   uri = URI.parse(redisUri) 
@@ -13,6 +14,23 @@ get '/getData' do
 end
 
 post '/getData' do
-  logger.info "getting data for #{params[:manifest]}"
-  { "http://fashionhack.com:4567/tumblr_mu5nuwmOMB1sk4zhuo1_500.jpg_707_500" => "http://www.betabrand.com/mens/pants/mens-japants-cargo-pants.html"}.to_json
+  data = request.body.read
+  logger.info "getting data, available: #{data}"
+  result = {}
+  manifest = JSON.parse data
+  logger.info "manifest is: #{manifest}"
+  manifest.each do |k|
+  	val = REDIS.get k
+  	next if val.nil?
+  	result[k] = val
+  end
+  logger.info result
+  result.to_json
+end
+
+post '/dataFor' do
+	url = params.fetch("product_url")
+	key = params.fetch("image_hash")
+	REDIS.set(key, url)
+	"ok"
 end
